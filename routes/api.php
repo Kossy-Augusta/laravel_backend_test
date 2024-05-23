2aa8793811d2b1f3725543b4f40eecff7889b184
Kossy-Augusta <kossyudogu@gmail.com>
2024-05-23 20:20:31 +0100

    created a route to access the register endpoint

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,8 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 // public Routes
-Route::post('/register');
+Route::post('/register', [AuthController::class, 'register']);
