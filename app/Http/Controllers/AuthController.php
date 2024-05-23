955ec2c1ec86d571b657b9aaf330324f4e602b0c
Kossy-Augusta <kossyudogu@gmail.com>
2024-05-23 20:55:56 +0100

    created login method used to login in and authenticate a user

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -22,4 +22,27 @@ class AuthController extends Controller
 
         return response($response, 201);
     }
+    //Login a registered user
+    public function login(Request $request){
+        $formFields = $request->validate([
+            'email' => 'required|string',
+            'password'=> 'required|string'
+        ]);
+        $user = User::where('email', $formFields['email'])->first();
+        // Check if password matches the stored password or email exists
+        if (!$user || !Hash::check($formFields['password'], $user->password)){
+            return response([
+                'message' => 'Invalid Credentials'
+            ], 401);
+        }
+        // generate token for the user and add 1 minute as expiration
+        $token = $user->createToken('myApiToken', ['*'], now()->addMinute())->plainTextToken;
+
+        $response = [
+            'user' => $user,
+            'token' => $token
+        ];
+
+        return response($response, 201);
+    }
 }
