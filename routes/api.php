4dce0570d81c311d58a60122caeeb52c493ce08b
Kossy-Augusta <kossyudogu@gmail.com>
2024-05-24 10:10:30 +0100

    created the show route to show a particular product

--- routes/api.php
+++ routes/api.php
@@ -9,6 +9,7 @@ use App\Http\Controllers\ProductController;
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 Route::get('/products', [ProductController::class, 'index']);
+Route::get('/products/{id}', [ProductController::class, 'show']);
 
 // Protected routes
 Route::group(['middleware' => ['auth:sanctum']],function () {
