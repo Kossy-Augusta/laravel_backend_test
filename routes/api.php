4166256fee03f5af76b5c3fe12452d54b258393f
Kossy-Augusta <kossyudogu@gmail.com>
2024-05-24 09:48:44 +0100

    added the index rout and the product creation route

--- routes/api.php
+++ routes/api.php
@@ -1,15 +1,17 @@
 <?php
 
-use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ProductController;
 
 // public Routes
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
+Route::get('/products', [ProductController::class, 'index']);
 
 // Protected routes
 Route::group(['middleware' => ['auth:sanctum']],function () {
     Route::post('/logout', [AuthController::class, 'logout']);
-    
+    Route::post('/products', [ProductController::class, 'store']);
 });
