b0c797e3cb11ea45fce1f2735c03ed81854bf46d
Kossy-Augusta <kossyudogu@gmail.com>
2024-05-28 13:02:13 +0100

    used gate to check if user is authorized to update and delete a product

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Models\Products;
+use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Gate;
 
 class ProductController extends Controller
 {
@@ -41,12 +43,6 @@ class ProductController extends Controller
 
     //  Update a product
     public function update($id, Request $request){
-        $product = Products::findorFail($id);
-
-        if($product->user_id !== auth()->id()){
-            return response()->json(['error_message'=>'Unauthorized access'], 403);
-        }
-
         $formFields = $request->validate([
             'name' => 'sometimes|required|string',
             'description' => 'sometimes|required|string',
@@ -54,6 +50,21 @@ class ProductController extends Controller
             'unit_price' => 'sometimes|required|decimal:2',
             'amount_sold' => 'sometimes|required|integer'
         ]);
+
+        $product = Products::findorFail($id);
+
+        // if($product->user_id !== auth()->id()){
+        //     return response()->json(['error_message'=>'Unauthorized access'], 403);
+        // }
+        
+        // use gate to check if user is authorized to make update
+        try{
+            Gate::authorize('update-product', $product);
+        } catch(AuthorizationException $e){
+            return response()->json([
+                'error_message' => $e->getMessage()
+            ], 403);
+        }
 
         $product->update($formFields);
         return response()->json([$product, 'message'=>'Product updated succesfully']);
@@ -63,8 +74,16 @@ class ProductController extends Controller
     public function destroy($id, Request $request){
         $product = Products::findorFail($id);
         // Check if user is authenticated and product belongs to user
-        if($product->user_id !== auth()->id()){
-            return response()->json(['error_message'=>'Unauthorized access'], 403);
+        // if($product->user_id !== auth()->id()){
+        //     return response()->json(['error_message'=>'Unauthorized access'], 403);
+        // }
+        // use gate to check if user is authorized to make update
+        try{
+            Gate::authorize('update-product', $product);
+        } catch(AuthorizationException $e){
+            return response()->json([
+                'error_message' => $e->getMessage()
+            ], 403);
         }
         $product->delete($product);
         return response()->json(['message'=>'Product was deleted succesfully']);
