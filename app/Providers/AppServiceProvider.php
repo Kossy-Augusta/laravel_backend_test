c7bacf09c13c704bce8448c3a8980244d8b572fd
Kossy-Augusta <kossyudogu@gmail.com>
2024-05-28 12:45:56 +0100

    defined a gate to check if user is authorized to make chenges to a product

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,10 @@
 
 namespace App\Providers;
 
+use App\Models\Products;
+
+use App\Models\User;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +23,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // update a product
+        Gate::define('update-product', function(User $user, Products $products){
+            return $user->id===$products->user_id;
+        });
     }
 }
