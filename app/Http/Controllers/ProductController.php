57cb24f1b8088334cd410e5dd5e21712c286e65a
Kossy-Augusta <kossyudogu@gmail.com>
2024-05-29 10:28:02 +0100

    refactored store, update and destroy method

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,9 +3,11 @@
 namespace App\Http\Controllers;
 
 use App\Models\Products;
-use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\Http\Request;
+use App\Http\Requests\CreateRequest;
+use App\Http\Requests\UpdateRequest;
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Auth\Access\AuthorizationException;
 
 class ProductController extends Controller
 {
@@ -20,17 +22,10 @@ class ProductController extends Controller
         return response()->json($products);
     }
     //create a new product
-    public function store(Request $request){
-        $formFields = $request->validate([
-            'name' => 'required|string',
-            'description' => 'required|string',
-            'quantity' => 'required|integer',
-            'unit_price' => 'required|decimal:2',
-            'amount_sold' => 'required|integer'
+    public function store(CreateRequest $request){
+        $validatedData = $request->validated();
 
-        ]);
-
-        $product = auth()->user()->products()->create($formFields);
+        $product = auth()->user()->products()->create($validatedData);
         return response()->json($product, 201);
 
     }
@@ -42,49 +37,26 @@ class ProductController extends Controller
     }
 
     //  Update a product
-    public function update($id, Request $request){
-        $formFields = $request->validate([
-            'name' => 'sometimes|required|string',
-            'description' => 'sometimes|required|string',
-            'quantity' => 'sometimes|required|integer',
-            'unit_price' => 'sometimes|required|decimal:2',
-            'amount_sold' => 'sometimes|required|integer'
-        ]);
+    public function update($id, UpdateRequest $request){
+        $validatedData = $request->validated() ;
 
         $product = Products::findorFail($id);
-
-        // if($product->user_id !== auth()->id()){
-        //     return response()->json(['error_message'=>'Unauthorized access'], 403);
-        // }
-        
-        // use gate to check if user is authorized to make update
-        try{
-            Gate::authorize('update-product', $product);
-        } catch(AuthorizationException $e){
-            return response()->json([
-                'error_message' => $e->getMessage()
-            ], 403);
-        }
-
-        $product->update($formFields);
+        $product->update($validatedData);
         return response()->json([$product, 'message'=>'Product updated succesfully']);
 
     }
     //  Delete a product
     public function destroy($id, Request $request){
         $product = Products::findorFail($id);
-        // Check if user is authenticated and product belongs to user
-        // if($product->user_id !== auth()->id()){
-        //     return response()->json(['error_message'=>'Unauthorized access'], 403);
-        // }
-        // use gate to check if user is authorized to make update
+        // use gate to check if user is authorized to delete a prooduct
         try{
-            Gate::authorize('update-product', $product);
+            Gate::authorize('destroy-product', $product);
         } catch(AuthorizationException $e){
             return response()->json([
                 'error_message' => $e->getMessage()
             ], 403);
         }
+        // if authorization passes, delete the product
         $product->delete($product);
         return response()->json(['message'=>'Product was deleted succesfully']);
 
