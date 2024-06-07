929334cfb2be7bcedca3e77f758608dc8978e14c
Kossy-Augusta <kossyudogu@gmail.com>
2024-06-07 12:33:43 +0100

    Updated the name attribute of the categories table to be unique

--- database/migrations/2024_06_07_112330_add_unique_constraint_to_column_in_categories.php
+++ database/migrations/2024_06_07_112330_add_unique_constraint_to_column_in_categories.php
@@ -11,11 +11,8 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('categories', function (Blueprint $table) {
-            $table->id();
-            $table->string('name')->unique();
-            $table->text('description')->nullabe();
-            $table->timestamps();
+        Schema::table('categories', function (Blueprint $table) {
+            $table->unique('name');
         });
     }
 
@@ -24,6 +21,8 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('categories');
+        Schema::table('categories', function (Blueprint $table) {
+            $table->dropUnique(['name']);
+        });
     }
 };
