9f58023fb819af2f4c0c27ae106bb2dd4863141a
Kossy-Augusta <kossyudogu@gmail.com>
2024-06-08 17:22:12 +0100

    added methods to update and delete categories

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Category;
 use Illuminate\Http\Request;
 use App\Http\Requests\CreateCategoryRequest;
+use App\Http\Requests\UpdateCategoryRequest;
 
 class CategoryController extends Controller
 {
@@ -25,5 +26,20 @@ class CategoryController extends Controller
 
         $category = Category::create($validatedData);
         return response()->json($category, 201);
+    }
+    public function update(UpdateCategoryRequest $request, $id)
+    {
+        $validatedData = $request->validated();
+        $category = Category::findOrFail($id);
+
+        $category->update($validatedData);
+        return response()->json([$category, "message" => "Category updated successfully"],201);
+    }
+
+    public function destroy($id)
+    {
+        $category = Category::findOrFail($id);
+        $category->delete($id);
+        return response("Category has been deleted");
     }
 }
